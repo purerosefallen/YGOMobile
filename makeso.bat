3a24604fc3b2781ba8c58d6937eb490794833ab2
purerosefallen <78877@qq.com>
2017-09-04 14:17:03 +0800

    build

--- makeso.bat
+++ makeso.bat
@@ -1,6 +1,20 @@
 @echo off
-cp -rf ../../ygopro-server/ygopro-server/servers/ygopro-222/ygopro/ocgcore Classes
-cd libcore
+echo Building data
+cp -rf ../../222DIY/git-build-server/ocgcore Classes
+cp -rf ../../222DIY/git-build-server/lflist.conf mobile/assets/data/core/3.5/config
+rm -rf mobile/assets/data/core/3.5/config/strings.conf
+cat ../../koishi_ocgupd/ygopro-7210srv/strings.conf ../../222DIY/git-build-server/expansions/strings.conf > mobile/assets/data/core/3.5/config/strings.conf
+cp -rf ../cards.cdb mobile/assets/data
+cp -rf ../script.zip mobile/assets/data
+echo Compiling core
+cd ../ndk
+setx Path %cd%
+cd ../ygopro-222DIY-mobile/libcore
 
 ndk-build -j4
+echo Pushing to GitHub
+cd ..
+git add . -A
+git commit -m build
+git push origin master
 pause
