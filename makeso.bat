7f6a7120c519f5a668ad97f9b465fb652ee82a89
purerosefallen <78877@qq.com>
2017-09-04 22:12:42 +0800

    database

--- makeso.bat
+++ makeso.bat
@@ -4,16 +4,30 @@ cp -rf ../../222DIY/git-build-server/ocgcore Classes
 cp -rf ../../222DIY/git-build-server/lflist.conf mobile/assets/data/core/3.5/config
 rm -rf mobile/assets/data/core/3.5/config/strings.conf
 cat ../../koishi_ocgupd/ygopro-7210srv/strings.conf ../../222DIY/git-build-server/expansions/strings.conf > mobile/assets/data/core/3.5/config/strings.conf
-cp -rf ../cards.cdb mobile/assets/data
 cp -rf ../script.zip mobile/assets/data
+echo Merging database
+
+mkdir databases
+cd databases
+cp -rf ../../../222DIY/git-build-server/expansions/222DIY.cdb .
+cp -rf ../../../koishi_ocgupd/ygopro-7210srv/cards.cdb .
+cp -rf ../../../koishi_ocgupd/ygopro-7210srv/expansions/pre-release.cdb .
+cp -rf ../../../koishi_ocgupd/ygopro-7210srv/expansions/pre-update.cdb .
+sqlite3 < ../merge.bat
+cat *.mg > merge
+rm -rf ../mobile/assets/data/cards.cdb
+sqlite3 ../mobile/assets/data/cards.cdb < merge
+cd ..
+rm -rf databases
+
 echo Compiling core
 cd ../ndk
 setx Path %cd%
 cd ../ygopro-222DIY-mobile/libcore
 
 ndk-build -j4
-echo Pushing to GitHub
 cd ..
+echo Pushing to GitHub
 git add . -A
 git commit -m build
 git push origin master
---
+++ merge.bat
@@ -0,0 +1,19 @@
+.open 222DIY.cdb
+.output 222DIY.mg
+.dump
+.output stdout
+
+.open cards.cdb
+.output cards.mg
+.dump
+.output stdout
+
+.open pre-release.cdb
+.output pre-release.mg
+.dump
+.output stdout
+
+.open pre-update.cdb
+.output update.mg
+.dump
+.output stdout
