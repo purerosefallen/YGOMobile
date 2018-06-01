da77cfc7912c46e289dc99fc31a78a0c54381fe3
purerosefallen <78877@qq.com>
2018-06-01 20:01:51 +0800

    copy windbot files

--- mobile/src/main/java/cn/garymb/ygomobile/Constants.java
+++ mobile/src/main/java/cn/garymb/ygomobile/Constants.java
@@ -57,6 +57,7 @@ public interface Constants {
     String CORE_SKIN_PATH = "textures";
     String CORE_SKIN_PENDULUM_PATH = CORE_SKIN_PATH + "/extra";
     String CORE_DECK_PATH = "deck";
+    String CORE_WINDBOT_PATH = "windbot";
     String CORE_EXPANSIONS ="expansions";
     String CORE_SINGLE_PATH = "single";
     String CORE_IMAGE_PATH = "pics";
