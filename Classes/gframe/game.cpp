8136e760f0f07ff1dfc1bab60991131cb93309b3
purerosefallen <78877@qq.com>
2018-06-09 08:33:43 +0800

    fix

--- Classes/gframe/game.cpp
+++ Classes/gframe/game.cpp
@@ -1916,6 +1916,9 @@ void Game::CloseDuelWindow() {
 	wReplaySave->setVisible(false);
 	stHintMsg->setVisible(false);
 	btnSideOK->setVisible(false);
+ 	btnSideShuffle->setVisible(false);
+ 	btnSideSort->setVisible(false);
+ 	btnSideReload->setVisible(false);
 	btnLeaveGame->setVisible(false);
 	btnSpectatorSwap->setVisible(false);
 	btnChainIgnore->setVisible(false);
